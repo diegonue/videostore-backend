26e7ab7ba436bc114344737cfac6d427f340e4b0
Diego N. <diego.nue@gmail.com>
2025-04-09 19:25:03 -0600

    Implementa entidad Movie con relaciones ManyToOne y ManyToMany

---
+++ src/main/java/com/videostore/modules/movie/model/Movie.java
@@ -0,0 +1,57 @@
+package com.videostore.modules.movie.model;
+
+import com.videostore.modules.actor.model.Actor;
+import com.videostore.modules.director.model.Director;
+import com.videostore.modules.genre.model.Genre;
+import com.videostore.modules.movietype.model.MovieType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "movies")
+@Data
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private String description;
+    private Double price;
+    private Double duration;
+    private Integer copyCount;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_type_id")
+    private MovieType movieType;
+
+    @ManyToMany
+    @JoinTable(
+            name = "movie_director",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "director_id")
+    )
+    private Set<Director> directors = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "movie_actor",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id")
+    )
+    private Set<Actor> actors = new HashSet<>();
+
+}
