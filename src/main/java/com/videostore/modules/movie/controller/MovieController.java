055296a593e36c6a36cea8d9d51c6f354fc352d2
Diego N. <diego.nue@gmail.com>
2025-04-10 12:01:10 -0600

    Implementa MovieController para Movie con endpoints

---
+++ src/main/java/com/videostore/modules/movie/controller/MovieController.java
@@ -0,0 +1,40 @@
+package com.videostore.modules.movie.controller;
+
+import com.videostore.modules.movie.dto.MovieDTO;
+import com.videostore.modules.movie.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<MovieDTO> create(@RequestBody MovieDTO dto) {
+        return ResponseEntity.ok(movieService.save(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieDTO>> getAll() {
+        return ResponseEntity.ok(movieService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDTO> getById(@PathVariable Integer id) {
+        MovieDTO dto = movieService.findById(id);
+        return dto != null ? ResponseEntity.ok(dto) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        movieService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
